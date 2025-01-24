be7e6c9a4ea5d810781f08744d49621588b24d99
Krzysztof Krystian Jankowski <kj@p1x.in>
2025-01-24 12:58:09 +0100

    src

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     
     # Load custom font
-    font_id = QFontDatabase.addApplicationFont("font.otf")
+    font_id = QFontDatabase.addApplicationFont("src/font.otf")
     if font_id != -1:
         font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
         app.setFont(QFont(font_family, 10))
